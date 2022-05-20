a20767f5e27e03dc8877f292970ef081a74637af
moshmage <moshmage@gmail.com>
2022-05-20 14:32:17 +0100

    Update parse-template.mjs

--- utils/parse-template.mjs
+++ utils/parse-template.mjs
@@ -3,7 +3,7 @@ import fs from 'fs';
 import path from 'path';
 
 export default function parseTemplate(fileName = "", context = {}, templateDir = "") {
-  templateDir = templateDir || process.env.TEMPLATE_DIRclear || "./src/templates/";
+  templateDir = templateDir || process.env.TEMPLATE_DIR || "./src/templates/";
 
   const template = handlebars.compile(fs.readFileSync(path.resolve(`${templateDir}${fileName}.hbs`), 'utf-8'), {noEscape: true});
   return template(context);
