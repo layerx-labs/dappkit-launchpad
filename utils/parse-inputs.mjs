eb79a9cc99d5044ede192db581d353d4146b1250
moshmage <moshmage@gmail.com>
2022-05-24 11:59:44 +0100

    use string template instead of adding

--- utils/parse-inputs.mjs
+++ utils/parse-inputs.mjs
@@ -10,6 +10,6 @@ export default function parseInputs(inputs, joiner = `, `, noType = false) {
   return inputs?.map((input, i) => 
             input.components?.length
               ? parseTemplate("object-argument", {params: parseOutput([input], `%content%`, true)})
-              : parseTemplate("argument", {inputName: input.name || 'v'+(i+1), type: !noType && getSolidityType(input.type)})
+              : parseTemplate("argument", {inputName: input.name || `v${i+1}`, type: !noType && getSolidityType(input.type)})
           )?.join(joiner);
 }
