dda7f3362987e367f43d79b50f95fe6c5d355df0
moshmage <moshmage@gmail.com>
2022-06-06 13:19:06 +0100

    fix workflows (#5)
    * uses nodejs
    
    * eq -> contain because endlines

--- test/make-function.mjs
+++ test/make-function.mjs
@@ -3,12 +3,12 @@ import {expect} from "chai";
 
 describe(`covers makeFunction()`, () => {
   it(`pure and SendMethod`, () => {
-    expect(makeFunction({stateMutability: "external", outputs: []}, false)).to.eq(`  (): ContractSendMethod\r\n`)
+    expect(makeFunction({stateMutability: "external", outputs: []}, false)).to.contain(`  (): ContractSendMethod`)
   });
 
   it(`nameless arg CallMethod`, () => {
-    expect(makeFunction({stateMutability: "pure", inputs: [{type: "uint"}], outputs: [{type: "uint"}]}, false)).to.eq(`  (arg1: number): ContractCallMethod<number>;\r\n`)
-  })
+    expect(makeFunction({stateMutability: "pure", inputs: [{type: "uint"}], outputs: [{type: "uint"}]}, false)).to.contain(`  (arg1: number): ContractCallMethod<number>;`)
+  });
 
   it(`nameless arg CallMethod with body`, () => {
     expect(makeFunction({stateMutability: "pure", name: "method", inputs: [{type: "uint"}], outputs: [{type: "uint"}]}, true))
