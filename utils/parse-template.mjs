c85e7f2b92007cb1a349dcc43371c5c886396ac6
moshmage <moshmage@gmail.com>
2022-08-01 15:01:00 +0100

    fix default place of templatesDir (#6)
    * uses nodejs
    
    * eq -> contain because endlines
    
    * use __dirname as fallback instead of ./src/

--- utils/parse-template.mjs
+++ utils/parse-template.mjs
@@ -1,10 +1,13 @@
 import handlebars from 'handlebars';
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default function parseTemplate(fileName = "", context = {}, templateDir = "") {
-  templateDir = templateDir || process.env.TEMPLATE_DIR || "./src/templates/";
+  templateDir = templateDir || process.env.TEMPLATE_DIR || `${__dirname}/../src/templates/`;
 
-  const template = handlebars.compile(fs.readFileSync(path.resolve(`${templateDir}${fileName}.hbs`), 'utf-8'), {noEscape: true});
+  const template = handlebars.compile(fs.readFileSync(path.resolve(path.join(templateDir, `${fileName}.hbs`)), 'utf-8'), {noEscape: true});
   return template(context);
 }
